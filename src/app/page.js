496cba7b25b6a9d5a8a16c991f4536e35c4b5244
ritikZangoh <ritik@zangoh.com>
2023-09-16 15:42:02 +0530

    .env added

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,11 @@
+/**
+ * Content Generation Next Page
+ * 
+ * This page is designed to be used as part of a web application that allows users to embed in their website for generate content dynamically.
+ * It includes features like character count, billing status, and customization options.
+ * 
+ * @component
+*/
 'use client'
 import React, { useState, useEffect } from 'react'
 import { useSearchParams } from 'next/navigation'
@@ -5,15 +13,18 @@ import ClipLoader from 'react-spinners/ClipLoader'
 import { Icon } from '@iconify/react'
 
 export default function Home() {
+  // State variables to manage various aspects of the component
   const [typeOfContent, setTypeOfContent] = useState('')
   const [message, setMessage] = useState('')
   const [charCount, setCharCount] = useState(0)
   const [responseMessage, setResponseMessage] = useState('')
-  const [responseWordCount, setResponseWordCount] = useState(0);
+  const [responseWordCount, setResponseWordCount] = useState(0)
   const [paidStatus, setPaidStatus] = useState(true) // State for billing status
   const [readOnly, setReadOnly] = useState(false) // State for readonly
   const [loading, setLoading] = useState(false)
+  const [waitForResponse, setWaitForResponce] = useState(false)
 
+  // Retrieve query parameters from the URL
   const searchParams = useSearchParams()
 
   const fontFamily = searchParams.get('fontfamily')
@@ -24,17 +35,16 @@ export default function Home() {
   const width = searchParams.get('width')
   const height = searchParams.get('height')
   const modalID = searchParams.get('key')
-  const [waitForResponse, setWaitForResponce] = useState(false)
 
   // To set the readOnly state
   useEffect(() => {
     setReadOnly(searchParams.get('modaltype') === 'read' ? true : false)
-    initializeChatBot();
+    initializeChatBot()
   }, [])
 
-  // To change state to unpaid
+  // To update status of billing
   useEffect(() => {
-    setBillingMessage()
+    updateBillingStatus()
   }, [paidStatus])
 
   // To update char count of message
@@ -42,8 +52,8 @@ export default function Home() {
     setCharCount(message.length)
   }, [message])
 
-  // Change state when bill is unpaid
-  function setBillingMessage() {
+  // To set billing status
+  function updateBillingStatus() {
     if (!paidStatus) {
       setResponseMessage(
         'Hey! This AI modal is having trouble, this is typically due to a billing issue. Please contact your site administrator for more info.',
@@ -55,40 +65,46 @@ export default function Home() {
   // To get billing status of user
   async function initializeChatBot() {
     try {
-      const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/initialize_chatbot',{
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_API_URL + '/api/initialize_chatbot',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ modalId: modalID }),
         },
-        body: JSON.stringify({ modalId: modalID }),
-      })
+      )
       const responseData = await response.json()
 
       if (response.status == 200) {
-        let paid = responseData.paid == 0 ? false : true;
+        let paid = responseData.paid == 0 ? false : true
         setPaidStatus(paid)
-        setTypeOfContent(responseData.contentType);
+        setTypeOfContent(responseData.contentType)
       }
     } catch (error) {
-      console.error('Error:', error)
       // Handle errors here
+      console.error('Error:', error)
     }
   }
 
   // To handle the generate content
   async function handleGenerateContent() {
-    if ( !message || waitForResponse) return
-    setWaitForResponce(true);
-    setLoading(true);
+    if (!message || waitForResponse) return
+    setWaitForResponce(true)
+    setLoading(true)
 
     try {
-      const responce = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/handle_request', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
+      const responce = await fetch(
+        process.env.NEXT_PUBLIC_API_URL + '/api/handle_request',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ modalId: modalID, msg: message }),
         },
-        body: JSON.stringify({ modalId: modalID, msg: message }),
-      })
+      )
 
       const responseData = await responce.json()
 
@@ -97,14 +113,17 @@ export default function Home() {
       } else {
         setResponseMessage(responseData.error)
       }
-      setResponseWordCount(responseData.msg.trim().split(/\s+/).length);
+      setResponseWordCount(responseData.msg.trim().split(/\s+/).length)
     } catch (error) {
+      // Handle error here
       console.log('Error: ' + error)
     }
-    setLoading(false);
-    setWaitForResponce(false);
+
+    setLoading(false)
+    setWaitForResponce(false)
   }
 
+  // Component render
   return (
     <div
       className="w-screen h-screen flex items-center justify-center"
@@ -133,7 +152,9 @@ export default function Home() {
             className="w-full bg-[#F2F2F2] mt-[10px] px-[18px] py-3 rounded-md"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
-            onKeyDown={(e) =>{ if(e.key === 'Enter') handleGenerateContent()}}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') handleGenerateContent()
+            }}
             disabled={!paidStatus}
             type="text"
             required
@@ -146,17 +167,17 @@ export default function Home() {
             style={{
               fontWeight: buttonWeight,
               background: '#' + buttonColor,
-              opacity: paidStatus? '1' : '0.5',
+              opacity: paidStatus ? '1' : '0.5',
             }}
           >
             Generate
-            {loading && 
+            {loading && (
               <Icon
                 className="h-6 w-10 scale-[2] text-purple-600 ml-2 inline-block"
                 style=""
                 icon="eos-icons:three-dots-loading"
               />
-            }
+            )}
           </button>
 
           <h2
@@ -177,7 +198,9 @@ export default function Home() {
             value={responseMessage}
             onChange={(e) => setResponseMessage(e.target.value)}
           ></textarea>
-          <p className="text-xs text-right text-[#868686] mt-1">Words: {responseWordCount}</p>
+          <p className="text-xs text-right text-[#868686] mt-1">
+            Words: {responseWordCount}
+          </p>
         </div>
       </div>
     </div>
